0a96ac3f2467b7d0342246da52e75a6050a17ba8
maheshwaran <maheshwaran@VOID>
2023-09-14 20:49:46 +0530

    Variable initialization and declaration

---
+++ TNSIF/src/day1/variable.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=5, b=5;
+        System.out.println(a+ " " + b);
+	}
+
+}
